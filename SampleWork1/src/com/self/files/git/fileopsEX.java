f2e8c6ff0e66fbe55d706909d2bf0267f2e6d5b5
Khushboo <khush23082012gmail.com>
2022-12-06 19:08:26 +0530

    a comment regarding text file creation added.

--- SampleWork1/src/com/self/files/git/fileopsEX.java
+++ SampleWork1/src/com/self/files/git/fileopsEX.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class fileopsEX {
 
 	public static void main(String[] args) {
+//this will add a txt file in samplework1 folder in c drive
 		File myFile = new File("file1.txt");
 		
 				try {
